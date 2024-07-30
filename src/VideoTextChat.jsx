9443b64bf7664874c81fcb35f1c45fa4e470c1ef
Abhiraj Sagar <abhirajsagar99@gmail.com>
2024-07-30 16:12:11 +0530

    Finalized Code Base

--- src/VideoTextChat.jsx
+++ src/VideoTextChat.jsx
@@ -1,200 +1,257 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Navbar from "./ui/Navbar";
-import { BiSend } from "react-icons/bi";
-import io from 'socket.io-client';
-import { useMediaQuery } from 'react-responsive';
+import { io } from "socket.io-client";
+import { useRef, useEffect, useState } from "react";
+import { FiVideo, FiVideoOff, FiMic, FiMicOff } from "react-icons/fi";
 
-
-export default function VideoTextChat() 
+const configuration = 
 {
-    let socket;
-    const hideAdsSideBar = useMediaQuery({ query: '(max-width: 820px)' });
-    const isAMobile = useMediaQuery({ query: '(max-width: 509px)' });
-    const localVideoTabletCSS = 
+    iceServers: 
+    [
+        {
+            urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+        },
+    ],
+    iceCandidatePoolSize: 10,
+};
+const socket = io("http://localhost:3000", { transports: ["websocket"] });
+
+let pc;
+let localStream;
+let startButton;
+let hangupButton;
+let muteAudButton;
+let remoteVideo;
+let localVideo;
+
+socket.on("message", (e) => 
+{
+    if (!localStream)
     {
-        position: 'absolute',
-        width:'150px',
-        height:'200px',
-        bottom: '0px',
-        right: '0px',
-        marginBottom: '8.5vh',
-        marginRight:'10px',
-    }
-    const localVideoMobileCSS =
-    {
-        position: 'absolute',
-        width:'70px',
-        height:'90px',
-        top: '0px',
-        right: '0px',
-        margin:'55px 5px',
+        console.log("Local Stream Not Ready Yet to Start Chatting");
+        return;
     }
 
-    const localVideoRef = useRef(null);
-    const remoteVideoRef = useRef(null);
-    const joinedRoomRef = useRef(false);
-    let peerConnections = {};
-    const constraints = { video: true, audio: true };
-    let localStream;
-    
-    
+    switch (e.type) 
+    {
+        case "offer":
+            handleOffer(e);
+            break;
+        case "answer":
+            handleAnswer(e);
+            break;
+        case "candidate":
+            handleCandidate(e);
+            break;
+        case "ready":
+            // A second tab joined. This tab will initiate a call unless in a call already.
+            if (pc)
+            {
+                console.log("Already in a Chat, Ignoring the recevied event");
+            }
+            else
+            {
+                makeOffer();
+            }
+            break;
+        case "bye":
+            if(pc) 
+            {
+                hangup();
+            }
+            break;
+        default:
+            console.log("Received Unknown Event, Can't be handled", e);
+            break;
+    }
+});
+
+async function makeOffer() 
+{
+    try
+    {
+        pc = new RTCPeerConnection(configuration);
+        pc.onicecandidate = (e) => 
+        {
+            const message = 
+            {
+                type: "candidate",
+                candidate: null,
+            };
+            if(e.candidate)
+            {
+                message.candidate = e.candidate.candidate;
+                message.sdpMid = e.candidate.sdpMid;
+                message.sdpMLineIndex = e.candidate.sdpMLineIndex;
+            }
+            socket.emit("message", message);
+        };
+
+        pc.ontrack = (receivedStream) => (remoteVideo.current.srcObject = receivedStream.streams[0]);
+        localStream.getTracks().forEach((track) => pc.addTrack(track, localStream));
+
+        const offer = await pc.createOffer();
+        socket.emit("message", { type: "offer", sdp: offer.sdp });
+        await pc.setLocalDescription(offer);
+    }
+    catch(error)
+    {
+        console.log('Error while establishing peer connection');
+    }
+}
+
+async function handleOffer(offer)
+{
+    if(pc)
+    {
+        console.log("Received a Offer While already in a Peer Connection, Ignoring the received offer");
+        return;
+    }
+    try
+    {
+        pc = new RTCPeerConnection(configuration);
+        pc.onicecandidate = (e) => 
+        {
+            const message = 
+            {
+                type: "candidate",
+                candidate: null,
+            };
+            if (e.candidate) 
+            {
+                message.candidate = e.candidate.candidate;
+                message.sdpMid = e.candidate.sdpMid;
+                message.sdpMLineIndex = e.candidate.sdpMLineIndex;
+            }
+            socket.emit("message", message);
+        };
+
+        pc.ontrack = (receivedStream) => (remoteVideo.current.srcObject = receivedStream.streams[0]);
+        localStream.getTracks().forEach((track) => pc.addTrack(track, localStream));
+        await pc.setRemoteDescription(offer);
+
+        const answer = await pc.createAnswer();
+        socket.emit("message", { type: "answer", sdp: answer.sdp });
+        await pc.setLocalDescription(answer);
+    }
+    catch(error)
+    {
+        console.log('Error While Creating An Answer to Received Offer',error);
+    }
+}
+
+async function handleAnswer(answer)
+{
+    if (!pc)
+    {
+        console.error("No Peer Connection Established to answer");
+        return;
+    }
+    try
+    {
+        await pc.setRemoteDescription(answer);
+    }
+    catch(error)
+    {
+        console.log('Error while receiving answer',error);
+    }
+}
+
+async function handleCandidate(candidate)
+{
+    if(!pc)
+    {
+        console.error("No Peer Connection Established to set its candidates");
+        return;
+    }
+
+    try
+    {
+        await pc.addIceCandidate(!candidate ? null : candidate); 
+    }
+    catch(error)
+    {
+        console.log('Error while adding ICE candidates',error);
+    }
+}
+async function hangup()
+{
+    if (pc)
+    {
+        pc.close();
+        pc = null;
+    }
+    localStream.getTracks().forEach((track) => track.stop());
+    localStream = null;
+    startButton.current.disabled = false;
+    hangupButton.current.disabled = true;
+    muteAudButton.current.disabled = true;
+}
+
+export default function VideoTextChat()
+{
+    startButton = useRef(null);
+    hangupButton = useRef(null);
+    muteAudButton = useRef(null);
+    localVideo = useRef(null);
+    remoteVideo = useRef(null);
+
     useEffect(() => 
     {
-        if(!joinedRoomRef.current)
-        {
-            socket = io('http://localhost:5000');
-            joinedRoomRef.current = true;
-            joinRoom();
-        }
-    
+        hangupButton.current.disabled = true;
+        muteAudButton.current.disabled = true;
     }, []);
 
-    async function joinRoom() 
+    const [audiostate, setAudio] = useState(false);
+
+    const startB = async () => 
     {
         try
         {
-            localStream = await navigator.mediaDevices.getUserMedia(constraints);
-            localVideoRef.current.srcObject = localStream;
-
-            socket.on('user-disconnected', userId => 
-            {
-                alert(`User ${userId} disconnected`);
-                if (peerConnections[userId])
-                {
-                    peerConnections[userId].close();
-                    delete peerConnections[userId];
-                }
-            });
-
-            socket.on('ice-candidate', (userId, iceCandidate) => 
-            {
-                console.log('received ICE candidate');
-                const peerConnection = peerConnections[userId];
-                if (peerConnection) {
-                    peerConnection.addIceCandidate(new RTCIceCandidate(iceCandidate))
-                        .then(() => { console.log('peer connection successful'); })
-                        .catch(error => console.warn('Error adding received ICE candidate:', error));
-                } else {
-                    console.log('no peer connection found to apply ice candidate');
-                }
-            });
-
-            socket.on('offer', (userId, offer) => 
-            {
-                console.log('received an offer');
-                if (!peerConnections[userId])
-                {
-                    createPeerConnection(userId);
-                }
-
-                peerConnections[userId].setRemoteDescription(new RTCSessionDescription(offer))
-                    .then(() => peerConnections[userId].createAnswer())
-                    .then(answer => peerConnections[userId].setLocalDescription(answer))
-                    .then(() => 
-                    {
-                        socket.emit('answer', userId, peerConnections[userId].localDescription);
-                        console.log('sent an answer');
-                    })
-                    .catch(error => console.error('Error handling offer:', error));
-            });
-
-            socket.on('answer', (userId, answer) => 
-            {
-                console.log('Received an answer');
-                const peerConnection = peerConnections[userId];
-                if (peerConnection)
-                {
-                    peerConnection.setRemoteDescription(new RTCSessionDescription(answer))
-                    .catch(error => console.error('Error setting remote description:', error));
-                    console.log('set answer');
-                }
-                else
-                {
-                    console.log('peer Connection not found to set answer');
-                }
-            });
-
-            socket.on('user-connected', userId => 
-            {
-                alert(`Setting up a peer connection between  else (${userId}) and you (${socket.id})`);
-                if (!peerConnections[userId])
-                {
-                    createPeerConnection(userId);
-                }
-                const peerConnection = peerConnections[userId];
-                peerConnection.createOffer().then((offer) => peerConnection.setLocalDescription(offer))
-                .then(() => 
-                {
-                    socket.emit('offer', userId, peerConnection.localDescription);
-                })
-                .catch(error => console.error('Error creating offer:', error));
-            });
-
-            socket.emit('get-partner');
-        } 
-        catch (error)
-        {
-            console.error('Error setting up connection', error);
+            localStream = await navigator.mediaDevices.getUserMedia({video: true,audio: { echoCancellation: true }});
+            localVideo.current.srcObject = localStream;
         }
-    }
+        catch(err)
+        {
+            console.log('Error while setting local Stream, allow camera and mic access',err);
+        }
 
-    function createPeerConnection(userId) 
+        startButton.current.disabled = true;
+        hangupButton.current.disabled = false;
+        muteAudButton.current.disabled = false;
+
+        socket.emit("message", { type: "ready" });
+    };
+
+    const hangB = async () => 
     {
-        const iceServers = {
-            iceServers: [
-                { urls: 'stun:stun.l.google.com:19302' },
-            ]
-        };
-        const peerConnection = new RTCPeerConnection(iceServers);
-    
-        peerConnection.onicecandidate = event => 
-        {
-            if (event.candidate) 
-            {
-                const iceCandidate = {
-                    sdpMid: event.candidate.sdpMid,
-                    sdpMLineIndex: event.candidate.sdpMLineIndex,
-                    candidate: event.candidate.candidate
-                };
-                socket.emit('ice-candidate', userId, iceCandidate);
-            }
-        };
-    
-        peerConnection.ontrack = (event) => 
-        {
-            remoteVideoRef.current.srcObject = event.streams[0];
-        };
-        
-        localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
+        hangup();
+        socket.emit("message", { type: "bye" });
+    };
 
-        peerConnections[userId] = peerConnection;
-        console.log(peerConnection);
-        return peerConnection;
+    function muteAudio()
+    {
+        localVideo.current.muted = audiostate;
+        setAudio(!audiostate);
     }
-    
 
     return (
-        <>
-            <div className='videoTextChat'>
-                <Navbar showProfile={false} showMenuBtn={false} />
-                <div className="horizontalContainer">
-                    <div className="textChat" style={hideAdsSideBar ? (isAMobile ? {} : {width: '40%',borderRight:'2px solid var(--blue)'}) : {width: '25%',borderRight:'2px solid var(--blue)'}}>
-                        <div className="chatMessages"></div>
-                        <div className="textChatInput">
-                            <input type="text" placeholder="Type your message here.." />
-                            <button>
-                                <BiSend />
-                            </button>
-                        </div>
-                    </div>
-                    <div className="videoChat" style={hideAdsSideBar ? (isAMobile ? {} : {width: '60%'}) : {width: '45%'}}>
-                        <video className="remoteVideo" ref={remoteVideoRef}  autoPlay playsInline ></video>
-                        <video className="localVideo" ref={localVideoRef} autoPlay  playsInline style={hideAdsSideBar ? (isAMobile ? localVideoMobileCSS : localVideoTabletCSS) : {}}></video>
-                        {hideAdsSideBar && <div className='inVideoAdsBanner'>Ads Banner</div>}
-                    </div>
-                    {!hideAdsSideBar && <div className='adsSideBar'>Some Ads Here...</div>} 
+        <div className="videoTextChat">
+            <main className="container">
+                <div className="video bg-main">
+                    <video ref={localVideo} className="video-item" autoPlay playsInline src=""></video>
+                    <video ref={remoteVideo} className="video-item" autoPlay playsInline src=""></video>
                 </div>
-            </div>
-        </>
+                <div className="btn">
+                    <button className="btn-item btn-start" ref={startButton} onClick={startB}>
+                        <FiVideo />
+                    </button>
+                    <button className="btn-item btn-end" ref={hangupButton} onClick={hangB}>
+                        <FiVideoOff />
+                    </button>
+                    <button className="btn-item btn-start" ref={muteAudButton}onClick={muteAudio}>
+                        {audiostate ? <FiMic /> : <FiMicOff />}
+                    </button>
+                </div>
+            </main>
+        </div>
     );
 }
